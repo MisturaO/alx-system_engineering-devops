3b94b0e37a6d197df776c010fe6ca0d50c02dcff
MisturaO <misturaojulari91@gmail.com>
2023-06-23 23:42:01 +0100

    create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp, set the mode and owners
 
 file {'/tmp/school':
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
