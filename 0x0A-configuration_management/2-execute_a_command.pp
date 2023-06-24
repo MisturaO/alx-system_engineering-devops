900ed8ad9ecd8a4e562cbe0e82a94b1e7c0cf79b
MisturaO <misturaojulari91@gmail.com>
2023-06-24 01:07:22 +0100

    kills a process named killmenow.

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow
 
-exec { 'pKill':
-  command  => 'pKill killmenow',
+exec { 'pkill':
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
