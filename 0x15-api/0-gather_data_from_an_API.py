a84cd0b7576ac758118f76913697d9335ab10f8b
MisturaO <misturaojulari91@gmail.com>
2023-07-25 13:10:02 +0100

    Gets data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script gathers data from an API using this REST API
+(https://jsonplaceholder.typicode.com/),for a given employeeID,
+returns information about his/her TODO list progress.
+
+This script accepts an integer as a parameter, which is the employee ID
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users/{}".format(argv[1])).json()
+    todos = requests.get(base_url + "todos",
+                        params={"userId/{}": argv[1]}).json()
+    completed_todo = [todo.get("title") for todo in todos if
+                      todo.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(users.get("name"), len("completed"), len("todos")))
+    [print("\t {}".format(c)) for c in completed_todo]
