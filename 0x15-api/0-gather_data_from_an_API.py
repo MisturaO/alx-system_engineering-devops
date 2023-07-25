d3eb4cac9fb5612ef1f10737d6b0dafba29c2dce
MisturaO <misturaojulari91@gmail.com>
2023-07-25 13:12:50 +0100

    Gets data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,13 +7,13 @@ returns information about his/her TODO list progress.
 This script accepts an integer as a parameter, which is the employee ID
 """
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(base_url + "users/{}".format(argv[1])).json()
+    users = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(base_url + "todos",
-                        params={"userId/{}": argv[1]}).json()
+                        params={"userId/{}": sys.argv[1]}).json()
     completed_todo = [todo.get("title") for todo in todos if
                       todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):"
