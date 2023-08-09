e5903df781c90ae994b21e149953e9547166bf03
MisturaO <misturaojulari91@gmail.com>
2023-08-09 14:52:36 +0100

    gets titles     of the first 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+"""
+Queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries Reddit API and gets titles
+    of the first 10 hot posts
+    Args:
+        subreddit
+    """
+    apiUrl = 'https://www.reddit.com/r/subreddit/top.json'.format(subreddit)
+    param = {"limit": 10}
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+
+    response = requests.get(apiUrl, params=param,
+                            headers=header, allow_redirects=False)
+    data = response.json()
+
+    if response.status_code >= 300:
+        print('None')
+
+    [print(child.get('data').get('title')) for child in
+     data.get('data').get('children')]
