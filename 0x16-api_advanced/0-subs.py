b731ccd576c9d6e9f94e2009b9d2995543bee950
MisturaO <misturaojulari91@gmail.com>
2023-08-09 12:19:24 +0100

    Gets the subscribers count of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,4 +18,4 @@ def number_of_subscribers(subreddit):
 
     if response.status_code >= 300:
         return 0
-    return response.json().get('data.subscribers')
+    return response.json().get('data').get('subscribers')
