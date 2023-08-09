4bb5ae5a6aab8abaa9b3dd5c21cee1c3cdf01bd4
MisturaO <misturaojulari91@gmail.com>
2023-08-09 12:20:47 +0100

    Gets the subscribers count of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,11 @@ subscribers (not active users, total subscribers) for a given subreddit
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers from a given subreddit
     Args:
-        subreddit
-        """
+        subreddit"""
 
     apiUrl = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {" User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
@@ -18,4 +18,4 @@ def number_of_subscribers(subreddit):
 
     if response.status_code >= 300:
         return 0
-    return response.json().get('data').get('subscribers')
+    return response.json().get('data.subscribers')
